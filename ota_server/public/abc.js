830aff0c02f853fd4a13fa75b7ded5e44ead6366
shiveshnavin <shiveshnavin@gmail.com>
2019-03-31 01:08:21 +0530

    Added LCD and multi touch

--- ota_server/public/abc.js
+++ ota_server/public/abc.js
@@ -5,50 +5,6 @@ load('api_sys.js')
 load('api_arduino_liquidcrystal_i2c.js')
 
 
-let lcd=LiquidCrystalI2C.create(0x27,16,2);
-lcd.init();
-lcd.clear();
-lcd.backlight();
-lcd.setCursor(0, 0);
-lcd.print("Hello, World!");
-
-print("LOADING...");
-let last={};
-RPC.addHandler('scan',function(req)
-{
-
-    RPC.call(RPC.LOCAL, 'I2C.Scan', { }, function (resp, ud) {
-
-
-        print("Scanned--->",resp);
-        last=resp;
-
-
-    }, null);
-        
-    return {messag:"Scaning...",last:last};
-});
-
-let bus = I2C.get();
-let lcd=function(addr,data)
-{
-    I2C.write(bus, addr,data, 2, true);  
-    Sys.usleep(40);
- 
-
-};
-RPC.addHandler('write',function(req)
-{
-    let ad=0x27;
-    lcd(ad,"\x38");
-    lcd(ad,"\x0C");
-    lcd(ad,"\x01");
-    lcd(ad,"\x80");
-    lcd(ad,"\x0f");
-   
-        
-    return {messag:"Wrote..."};
-});
  
 Cfg.set({i2c:{
     "enable": true,
@@ -58,9 +14,118 @@ Cfg.set({i2c:{
   }});
 
 
+let lcd=LiquidCrystalI2C.create(0x27,20,4); 
+lcd.init();
+lcd.backlight();
+lcd.setCursor(0, 0);
+lcd.clear();  
+lcd.print("! Sab Moh Maya Hai !");
+
+let printRow=function(op,row)
+{
+  
+    lcd.setCursor(0, row);
+    lcd.print("                    "); 
+    lcd.setCursor(0, row); 
+    lcd.print(op);
+
+};
+ 
+RPC.addHandler('write',function(req)
+{
+   
+    lcd.setCursor(0, 2);
+    lcd.print("                    "); 
+    lcd.setCursor(0, 1);
+    lcd.print("                    "); 
+    lcd.setCursor(0, 2);
+    if(req.data!==undefined)
+        lcd.print(req.data);
+    
+    return {messag:"Wrote..."};
+});
 
 
 
+
+load('api_esp32_touchpad.js'); 
+let lastTouch=0; 
+
+let time=0;
+let count=0;
+let on_touch=function(nn)
+{ 
+  
+
+
+    //print("Interrupt");
+    let ct=Timer.now();
+    if((ct-time)>1.2) 
+    {
+        let tn=0;
+        let tt=[TouchPad.readFiltered(ts0),TouchPad.readFiltered(ts1),TouchPad.readFiltered(ts2)];
+    
+        for(let i=0;i<tt.length;i++)
+        {
+            //print("Thr ",nonT[i]," val ",tt[i]," st ",st);
+            print("touched ",TouchPad.getStatus());
+            if(tt[i]>nonT[i])
+            {
+                tn=i;
+               // break;
+            }
+            tn= Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+;
+            TouchPad.clearStatus();
+        } 
+
+        print('Stat ',tn,' Click ');
+        count++;
+        printRow(""+JSON.stringify(count)+" bar chua",3)
+        printRow("Tch btn "+JSON.stringify(tn)+" tap kia",2)
+    }
+    time=ct;
+};
+
+ 
+TouchPad.init();
+TouchPad.filterStart(10);
+TouchPad.setMeasTime(0x1000, 0xffff);
+TouchPad.setVoltage(TouchPad.HVOLT_2V4, TouchPad.LVOLT_0V8, TouchPad.HVOLT_ATTEN_1V5);
+
+let nonT=[0,0,0];
+ 
+ 
+
+let ts0 = TouchPad.GPIO[4]; TouchPad.config(ts0, 0);
+Sys.usleep(100000);   
+nonT[0]= TouchPad.readFiltered(ts0) * 2 / 3; 
+TouchPad.setThresh(ts0, nonT[0] );
+
+
+let ts1 = TouchPad.GPIO[27]; TouchPad.config(ts1, 0);
+Sys.usleep(100000);   
+nonT[1]= TouchPad.readFiltered(ts1) * 2 / 3; 
+TouchPad.setThresh(ts1, nonT[1]);
+
+
+let ts2 = TouchPad.GPIO[13]; TouchPad.config(ts2, 0);
+Sys.usleep(100000);   
+nonT[2]= TouchPad.readFiltered(ts2)* 2 / 3; 
+TouchPad.setThresh(ts2, nonT[2] );
+
+
+ 
+
+
+TouchPad.isrRegister(function(st) {
+
+   
+    on_touch(st);
+
+}, null);
+TouchPad.intrEnable();
+ 
 
 
 
